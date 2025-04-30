e5f90c7c17cb2daf7187adec2c4896d46128772d
nastyaNSK <48093333+nastyaNSK@users.noreply.github.com>
2025-04-30 11:04:08 +0700

    Add files via upload

---
+++ .settings.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'controllers' => [
+        'value' => [
+            'namespaces' => [
+                '\\Mozaika\\Delivery\\Controller' => 'api',
+            ],
+        ],
+        'readonly' => true,
+    ]
+];
---
+++ include.php
@@ -0,0 +1,3 @@
+<?php
+Bitrix\Main\Loader::includeModule('mozaika.devenv');
+Mozaika\DevEnv\DevUtils::loadModules(['iblock', 'sale', 'catalog', 'mozaika.dadata']);
